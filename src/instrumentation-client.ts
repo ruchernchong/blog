bde835ba1fd876ffcbaea04c9d8ce7ca83adb9c7
Ru Chern Chong <hello@ruchern.dev>
2026-05-31 17:57:24 +0800

    feat: migrate analytics to @posthog/next package

--- src/instrumentation-client.ts
+++
@@ -1,7 +0,0 @@
-import posthog from "posthog-js";
-
-posthog.init(process.env.NEXT_PUBLIC_POSTHOG_PROJECT_TOKEN as string, {
-  api_host: "/ingest",
-  ui_host: "https://eu.posthog.com",
-  defaults: "2026-01-30",
-});
