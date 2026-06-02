a91e5c59be465a9e8dd395eaf9fa2d15c11bf5f6
Ru Chern Chong <hello@ruchern.dev>
2026-06-02 12:05:39 +0800

    refactor: reduce duplicated code

--- apps/web/src/data/companies.ts
+++ apps/web/src/data/companies.ts
@@ -1,21 +1,34 @@
 import type { Company } from "@/types";
 
+type SingleRoleCompany = Omit<Company, "roles"> & {
+  roleTitle?: string;
+};
+
+const withSingleRole = ({
+  roleTitle,
+  ...company
+}: SingleRoleCompany): Company => ({
+  ...company,
+  roles: [
+    {
+      title: roleTitle ?? company.title,
+      dateStart: company.dateStart,
+      dateEnd: company.dateEnd,
+    },
+  ],
+});
+
 const companies: Company[] = [
-  {
+  withSingleRole({
     name: "DBS Bank",
     title: "Application Developer",
     logo: "/companies/logo-dbs.svg",
     dateStart: "Apr 2021",
     location: "Singapore",
     url: "https://dbs.com.sg",
-    roles: [
-      {
-        title: "Application Developer (Fullstack)",
-        dateStart: "Apr 2021",
-      },
-    ],
-  },
-  {
+    roleTitle: "Application Developer (Fullstack)",
+  }),
+  withSingleRole({
     name: "Singtel",
     title: "Software Engineer",
     logo: "/companies/logo-singtel.svg",
@@ -23,15 +36,8 @@ const companies: Company[] = [
     dateEnd: "Dec 2020",
     location: "Singapore",
     url: "https://shop.singtel.com",
-    roles: [
-      {
-        title: "Software Engineer",
-        dateStart: "Aug 2018",
-        dateEnd: "Dec 2020",
-      },
-    ],
-  },
-  {
+  }),
+  withSingleRole({
     name: "Sproud",
     title: "Software Developer",
     logo: "/companies/logo-sproud.png",
@@ -39,15 +45,8 @@ const companies: Company[] = [
     dateEnd: "Aug 2018",
     location: "Singapore",
     url: "https://sproud.biz",
-    roles: [
-      {
-        title: "Software Developer",
-        dateStart: "Feb 2017",
-        dateEnd: "Aug 2018",
-      },
-    ],
-  },
-  {
+  }),
+  withSingleRole({
     name: "The University of Queensland",
     title: "Teaching Assistant",
     logo: "/companies/logo-uq.svg",
@@ -55,14 +54,7 @@ const companies: Company[] = [
     dateEnd: "Jun 2016",
     location: "Brisbane, Queensland, Australia",
     url: "https://www.eait.uq.edu.au",
-    roles: [
-      {
-        title: "Teaching Assistant",
-        dateStart: "Feb 2016",
-        dateEnd: "Jun 2016",
-      },
-    ],
-  },
+  }),
 ];
 
 export default companies;
