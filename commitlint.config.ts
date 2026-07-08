9fa33dbc7fdacd7cf151f8539955ceaaeb505b65
Ru Chern Chong <hello@ruchern.dev>
2026-07-08 19:24:29 +0800

    chore: update commitlint configuration

--- commitlint.config.ts
+++ commitlint.config.ts
@@ -2,6 +2,23 @@ import type { UserConfig } from "@commitlint/types";
 
 const config: UserConfig = {
   extends: ["@commitlint/config-conventional"],
+  parserPreset: {
+    presetConfig: {
+      types: [
+        { type: "feat", section: "Features" },
+        { type: "fix", section: "Bug Fixes" },
+        { type: "perf", section: "Performance Improvements" },
+        { type: "revert", section: "Reverts" },
+        { type: "docs", section: "Documentation" },
+        { type: "style", section: "Styles" },
+        { type: "refactor", section: "Code Refactoring" },
+        { type: "test", section: "Tests" },
+        { type: "build", section: "Build System" },
+        { type: "ci", section: "Continuous Integration" },
+        { type: "chore", section: "Maintenance", hidden: true },
+      ],
+    },
+  },
   rules: {
     "header-max-length": [2, "always", 72],
     "scope-empty": [2, "always"],
@@ -12,5 +29,4 @@ const config: UserConfig = {
     ],
   },
 };
-
 export default config;
